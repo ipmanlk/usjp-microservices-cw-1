bfc32ae64fa8d59328041d737a0d60c5207efe68
ipmanlk <io@navinda.xyz>
2023-04-23 19:56:49 +0530

    fix: missing annotations

--- src/main/java/xyz/navinda/ms1/repository/StudentRepository.java
+++ src/main/java/xyz/navinda/ms1/repository/StudentRepository.java
@@ -3,9 +3,10 @@ package xyz.navinda.ms1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import xyz.navinda.ms1.entity.Student;
 import java.util.List;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM Student s WHERE s.firstName LIKE %:name% OR s.lastName LIKE %:name%")
     List<Student> findByName(@Param("name") String name);
